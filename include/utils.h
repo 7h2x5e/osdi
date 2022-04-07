64a6df11bfb52eb4a63ae72903034f61bbb62401
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2022-04-07 19:07:56 +0800

    Implement reboot command

---
+++ include/utils.h
@@ -0,0 +1,7 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+void reset(int tick);
+void cancel_reset();
+
+#endif
--- src/shell.c
+++ src/shell.c
@@ -2,6 +2,7 @@
 #include "c_string.h"
 #include "time.h"
 #include "uart.h"
+#include "utils.h"
 
 #define LINEFEED 0x0A
 #define BUFFER_MAX_SIZE 80
@@ -10,12 +11,20 @@ void help() {
   uart_printf("help: print all available commands\n");
   uart_printf("hello: print Hello world!\n");
   uart_printf("timestamp: print current timestamp\n");
+  uart_printf("reboot: reboot rpi3 (doesn't work on QEMU)\n");
   uart_printf("exit: exit shell\n");
 }
 
 void hello() { uart_printf("Hello world\n"); }
 
 void timestamp() { uart_printf("[%f]\n", getTime()); }
+
+void reboot() {
+  uart_printf("Rebooting...\n");
+  reset(50);
+  while (1)
+    ; // should not return
+}
 
 void exit() { uart_printf("entering infinite loop..."); }
 
@@ -27,6 +36,8 @@ int search_command(char *buffer) {
     help();
   } else if (!strcmp(buffer, "timestamp")) {
     timestamp();
+  } else if (!strcmp(buffer, "reboot")) {
+    reboot();
   } else if (!strcmp(buffer, "exit")) {
     exit();
     ret = -1;
@@ -39,7 +50,7 @@ int search_command(char *buffer) {
 void shell() {
   char c, buffer[BUFFER_MAX_SIZE];
   unsigned int count = 0;
-  uart_printf("\n---Raspberry PI 3 B+---\n ~$ ");
+  uart_printf("\n---Raspberry PI 3 B+---\n~$ ");
   while ((c = uart_getc())) {
     switch (c) {
     case LINEFEED:
