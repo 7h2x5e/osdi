6901d23c67ce503bc30f7bcea0e78cd062f38a38
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2022-04-16 22:45:31 +0800

    Add atoi & htoi function

--- src/c_string.c
+++ src/c_string.c
@@ -16,6 +16,43 @@ int strcmp(const char *p1, const char *p2) {
   return c1 - c2;
 }
 
+int atoi(const char *str, int *dst) {
+  long int sum = 0;
+  char c;
+  int sign = 0;
+  if (*str == '-') {
+    str++;
+    sign = 1;
+  }
+  while ((c = *str++)) {
+    c -= '0';
+    if (c > 9 || c < 0)
+      return -1; // return -1 on failure
+    sum *= 10;
+    sum += c;
+  }
+  *dst = sign == 1 ? (int)(-sum) : (int)sum;
+  return 0;
+}
+
+int htoi(const char *str, int *dst) {
+  long int sum = 0;
+  char c, d;
+  while ((c = *str++)) {
+    if (c >= 'a' && c <= 'e') {
+      d = c - 'a' + 10;
+    } else if (c >= 'A' && c <= 'E') {
+      d = c - 'A' + 10;
+    } else if (c >= '0' && c <= '9') {
+      d = c - '0';
+    } else
+      return -1; // return -1 on failure
+    sum = (sum << 4) | d;
+  }
+  *dst = (int)sum;
+  return 0;
+}
+
 /*  bztsrc: raspi3-tutorial/12_printf/sprintf.c */
 static inline char *itoa(char *tmpstr, long int arg) {
   int sign = 0, i = 18;
