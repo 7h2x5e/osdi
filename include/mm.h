8450c8a4104b3689f62462b1dd47ac9380258218
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2022-11-18 05:01:00 +0800

    map kernel to the upper address space

---
+++ include/mm.h
@@ -0,0 +1,6 @@
+#ifndef _MM_H
+#define _MM_H
+
+#define KERNEL_VIRT_BASE 0xFFFF000000000000
+
+#endif
--- include/peripherals/base.h
+++ include/peripherals/base.h
@@ -1,6 +1,9 @@
 #ifndef BASE_H
 #define BASE_H
 
-#define MMIO_BASE 0x3F000000
-#define LOCAL_PERIPHERAL_BASE 0x40000000
+#include <include/mm.h>
+
+#define MMIO_BASE (KERNEL_VIRT_BASE | 0x3F000000)
+#define LOCAL_PERIPHERAL_BASE (KERNEL_VIRT_BASE | 0x40000000)
+
 #endif
