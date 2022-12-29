ec750f52a19a432c49b0ad4025f52d0ba6ae9b7f
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2022-12-30 01:48:19 +0800

    add user library and embed compiled binary to kernel

---
+++ user/ulib.h
@@ -0,0 +1,16 @@
+#ifndef ULIB_H
+#define ULIB_H
+
+#include "include/syscall.h"
+#include "include/stdlib.h"
+#include "include/stdio.h"
+#include "include/string.h"
+#include <include/signal.h>
+#include "include/types.h"
+
+#endif
+
+
+
+
+
