4502bdafd010d885f59d0dfb5821375b223360a2
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2023-01-02 19:56:12 +0800

    add test case for page fault handler

--- user/main.c
+++ user/main.c
@@ -8,6 +8,7 @@ void delay(int period)
 
 int main()
 {
+    printf("%h\n", *((char *) 0xffff000000000000)); /* Data abort */
     int count = 10;
     while (count) {
         printf("[PID %d] Hello world x%d\n", get_taskid(), count--);
