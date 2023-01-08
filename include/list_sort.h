be6eb16d14e45629a1a6c0a0b13cc0b333e3555e
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2023-01-09 01:49:38 +0800

    add list_sort

---
+++ include/list_sort.h
@@ -0,0 +1,16 @@
+#ifndef LIST_SORT_H
+#define LIST_SORT_H
+
+#include <include/types.h>
+
+struct list_head;
+
+typedef int
+    __attribute__((nonnull(2, 3))) (*list_cmp_func_t)(void *,
+                                                      const struct list_head *,
+                                                      const struct list_head *);
+
+__attribute__((nonnull(2, 3))) void list_sort(void *priv,
+                                              struct list_head *head,
+                                              list_cmp_func_t cmp);
+#endif
