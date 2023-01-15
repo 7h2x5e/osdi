5a670fa174148693f701c6d34de84f60226614e6
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2023-01-15 21:25:21 +0800

    fix typos

--- user/ulib.h
+++ user/ulib.h
@@ -1,16 +1,12 @@
 #ifndef ULIB_H
 #define ULIB_H
 
-#include "include/syscall.h"
-#include "include/stdlib.h"
-#include "include/stdio.h"
-#include "include/string.h"
+#include <include/syscall.h>
+#include <include/stdlib.h>
+#include <include/stdio.h>
+#include <include/string.h>
 #include <include/signal.h>
-#include "include/types.h"
+#include <include/types.h>
+#include <include/mman.h>
 
 #endif
-
-
-
-
-
