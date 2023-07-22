3ca9c7764dd7d708b8710a12d97b207750f4258a
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2023-07-22 15:25:16 +0800

    use ANSI colors in kernel log

--- include/kernel_log.h
+++ include/kernel_log.h
@@ -4,27 +4,32 @@
 #include <include/printk.h>
 #include <include/task.h>
 
-#define LOG_LEVEL 3
+#define LOG_LEVEL 0
 
-#define KERNEL_LOG_DEBUG(M, ...) \
-    printk("[kernel] pid %d: " M "\n", do_get_taskid(), ##__VA_ARGS__)
-#define KERNEL_LOG_INFO(M, ...) printk("[kernel] " M "\n", ##__VA_ARGS__)
-#define KERNEL_LOG_ERROR(M, ...) \
-    printk("[kernel] pid %d: " M "\n", do_get_taskid(), ##__VA_ARGS__)
+#define KERNEL_LOG_TRACE(M, ...)                      \
+    printk("\33[0;33m[kernel][trace][%d]\33[0;39m " M \
+           " \33[0;33m[in %s at %s:%d]\33[0m\n",      \
+           do_get_taskid(), ##__VA_ARGS__, __FUNCTION__, __FILE__, __LINE__)
+#define KERNEL_LOG_DEBUG(M, ...)                                   \
+    printk("\33[0;32m[kernel][debug][%d]\33[0;39m " M " \33[0m\n", \
+           do_get_taskid(), ##__VA_ARGS__)
+#define KERNEL_LOG_INFO(M, ...)                                    \
+    printk("\33[0;37m[kernel][info ][%d]\33[0;39m " M " \33[0m\n", \
+           do_get_taskid(), ##__VA_ARGS__)
 
 #if LOG_LEVEL < 3
+#undef KERNEL_LOG_TRACE
+#define KERNEL_LOG_TRACE(M, ...)
+#endif
+
+#if LOG_LEVEL < 2
 #undef KERNEL_LOG_DEBUG
 #define KERNEL_LOG_DEBUG(M, ...)
 #endif
 
-#if LOG_LEVEL < 2
+#if LOG_LEVEL < 1
 #undef KERNEL_LOG_INFO
 #define KERNEL_LOG_INFO(M, ...)
-#endif
-
-#if LOG_LEVEL < 1
-#undef KERNEL_LOG_ERROR
-#define KERNEL_LOG_ERROR(M, ...)
 #endif
 
 #endif
