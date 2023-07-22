1e3d2432de635030ed16e33a4162b25d0d439f4f
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2023-07-22 15:44:05 +0800

    add example for CoW

--- user/main.c
+++ user/main.c
@@ -8,22 +8,14 @@ void delay(int period)
 
 int main()
 {
-    void *addr;
-    if (MAP_FAILED != (uint64_t) (addr = mmap((void *) 0xdeadbeef, 4097,
-                                              PROT_READ | PROT_WRITE,
-                                              MAP_ANONYMOUS, NULL, 0))) {
-        printf("[user] mmap a new region, start at 0x%x\n", addr);
-        // munmap(addr, 8193);
-    }
+    int64_t count = 0;
 
-    int64_t pid = fork();
-    if (pid == -1) {
-        printf("[user] fork failed\n");
-    } else if (pid) {
-        printf("[user] parent process, get child process pid = %d\n", pid);
-    } else {
-        printf("[user] child process, pid = %d\n", pid);
+    while (count++ < 2) {
+        if (fork() == -1) {
+            printf("fork failed!\n");
+            break;
+        }
+        printf("pid=%d, count=%d\n", get_taskid(), count);
     }
-
     return 0;
 }
