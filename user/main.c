27ffde39bdfb0239361951f5c3c9d93cb4127199
7h2x5e <47744374+7h2x5e@users.noreply.github.com>
2023-12-19 07:05:52 +0800

    add example for mount and chdir

--- user/main.c
+++ user/main.c
@@ -8,14 +8,29 @@ void delay(int period)
 
 int main()
 {
-    int64_t count = 0;
+    char buf[32];
+    int sz, fd, ret;
 
-    while (count++ < 2) {
-        if (fork() == -1) {
-            printf("fork failed!\n");
-            break;
-        }
-        printf("pid=%d, count=%d\n", get_taskid(), count);
-    }
+    mkdir("mnt");
+    ret = mount("tmpfs", "mnt", "tmpfs");
+    fd = open("mnt/a.txt", O_CREAT);
+    sz = write(fd, "Hello world", 11);
+    printf("write %d bytes to fd %d\n", sz, fd);
+    close(fd);
+
+    memset(buf, 0, sizeof(buf));
+    chdir("mnt");
+    fd = open("a.txt", 0);
+    sz = read(fd, buf, 11);
+    printf("read %d bytes from fd %d: %s\n", sz, fd, buf);
+    close(fd);
+
+    memset(buf, 0, sizeof(buf));
+    chdir("..");
+    fd = open("a.txt", 0);
+    sz = read(fd, buf, 11);
+    printf("read %d bytes from fd %d: %s\n", sz, fd, buf);
+    close(fd);
+
     return 0;
 }
